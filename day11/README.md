0f8296c0980de84674a0b6e75a84163942ebe71f
Humza Iqbal <humzaiqbal@berkeley.edu>
2017-03-15 18:15:00 -0700

    adding day 11 material

---
+++ day11/README.md
@@ -0,0 +1,10 @@
+#Day 11
+## Decision Trees
+[Decision Tree Notebook](https://github.com/kaggledecal/sp17/blob/master/day10/knn.ipynb)
+
+
+
+
+[Slides](https://docs.google.com/presentation/d/1E3xTsMv4DZ5_c9BVN4KQ-qQ3gXh__TCybWL52Nt0yXo/edit#slide=id.p)
+
+[Video]
---
+++ day11/calc_gini.py
@@ -0,0 +1,18 @@
+import numpy as np
+"""
+In the below function you will be implementing a function
+to calculate the gini impurity for a given collection of labels 
+below are some examples to test how your function behaves
+"""
+def gini_index(classes):
+	raise Exception('Not implemented')
+
+
+"""
+Uncomment these tests to see how 
+your function behaves
+
+"""
+#gini_index([0, 0, 0, 0,0, 0]) -> 0
+#gini_index([0, 1, 0, 1, 0, 1]) -> 0.5
+
